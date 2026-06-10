cc34311a1e7db9b1513f677b535fe2a3126ff3b6
Harshit Yadav <official.harshit@outlook.com>
2026-06-10 12:03:24 +0530

    Add initial implementation of ApiClient with a greeting message

---
+++ Services/ApiClient.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class ApiClient
+{
+    public static void Main()
+    {
+        //Console.WriteLine("Hello world!");
+        Console.Write("Congratulations!");
+        Console.Write(" ");
+        Console.Write("You wrote your first lines of code.");
+    }
+}
